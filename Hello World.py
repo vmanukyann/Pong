bd9a4fcd95659e5a42ef5b87a77b7f5192e932e0
Vazgen Manukyan <125108598+vmanukyann@users.noreply.github.com>
2023-10-11 22:17:54 -0400

    New Music Features

--- Hello World.py
+++ Hello World.py
@@ -1,91 +1,116 @@
 import pygame
-pygame.init()
+import threading
+from pygame.locals import USEREVENT
 
-# Get the screen dimensions
-screen_info = pygame.display.Info()
-width, height = screen_info.current_w, screen_info.current_h
+# Define the file path as a variable
+audio_file_path = r"C:\Users\kmanukya\Downloads\The-Weeknd-Blinding-Lights.mp3"
 
-# Pong Game Display in Fullscreen
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
-pygame.display.set_caption("Pong Game")
+# Initialize the Pygame mixer and load the audio
+pygame.mixer.init()
+pygame.mixer.music.load(audio_file_path)
 
+# Create a custom event to handle audio looping
+audio_end_event = pygame.event.Event(USEREVENT + 1)
 
-# Colors
-white = (255, 255, 255)
-blue = (0, 0, 255)  # Define the blue color
+def play_audio():
+    pygame.mixer.music.play(-1)  # Play the audio in an infinite loop
 
-# Pong Ball Dimensions
-ball_width = 50
-ball_height = 50
+def main():
+    pygame.init()
 
-# Create the ball (a rectangular surface)
-ball = pygame.Surface((ball_width, ball_height))
-ball.fill(blue)  # Fill the ball with the blue color
+    # Get the screen dimensions
+    screen_info = pygame.display.Info()
+    width, height = screen_info.current_w, screen_info.current_h
 
-# Position of the ball
-ball_x, ball_y = 600, 250  # Initial position of the ball
+    # Pong Game Display in Fullscreen
+    screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+    pygame.display.set_caption("Pong Game")
 
-# Dimensions of Slide A
-Slide1_width = 30
-Slide1_height = 250
+    # Set background color to black
+    background_color = (0, 0, 0)
+    screen.fill(background_color)
 
-# Create Slide A (a rectangular surface)
-Slide = pygame.Surface((Slide1_width, Slide1_height))
-Slide.fill(blue)  # Fill Slide A with the blue color
+    # Set red color
+    red = (255, 0, 0)
 
-# Initial position of Slide A for motion
-Slide_x, Slide_y = 20, 150
+    # Pong Ball Dimensions
+    ball_width = 50
+    ball_height = 50
 
-# Dimensions of Slide B
-Slide2_width = 30
-Slide2_height = 250
+    # Create the ball (a rectangular surface)
+    ball = pygame.Surface((ball_width, ball_height))
+    ball.fill(red)  # Fill the ball with red
 
-# Create Slide B (a rectangular surface)
-Slide2 = pygame.Surface((Slide2_width, Slide2_height))
-Slide2.fill(blue)  # Fill Slide B with the blue color
+    # Position of the ball
+    ball_x, ball_y = 600, 250  # Initial position of the ball
 
-# Initial position of Slide B for motion
-Slide2_x, Slide2_y = 1220, 150
+    # Dimensions of Slide A
+    Slide1_width = 30
+    Slide1_height = 250
 
-# Running the Game
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # Create Slide A (a rectangular surface)
+    Slide = pygame.Surface((Slide1_width, Slide1_height))
+    Slide.fill(red)  # Fill Slide A with red
 
-    # Event handling for motion of Slides
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:  # Slide A goes up
-        Slide_y -= 0.5
-    if keys[pygame.K_s]:  # Slide A goes down
-        Slide_y += 0.5
-    if keys[pygame.K_UP]:  # Slide B goes up
-        Slide2_y -= 0.5
-    if keys[pygame.K_DOWN]:  # Slide B goes down
-        Slide2_y += 0.5
+    # Initial position of Slide A for motion
+    Slide_x, Slide_y = 20, 150
 
-    #Motion of Ball
-    
+    # Dimensions of Slide B
+    Slide2_width = 30
+    Slide2_height = 250
 
-    # Ensure that the slides stay within the screen boundaries
-    Slide_y = max(0, min(Slide_y, height - Slide1_height))
-    Slide2_y = max(0, min(Slide2_y, height - Slide2_height))
+    # Create Slide B (a rectangular surface)
+    Slide2 = pygame.Surface((Slide2_width, Slide2_height))
+    Slide2.fill(red)  # Fill Slide B with red
 
-    # Clear the screen
-    screen.fill(white)
+    # Initial position of Slide B for motion
+    Slide2_x, Slide2_y = 1220, 150
 
-    # Draw the ball on the screen
-    screen.blit(ball, (ball_x, ball_y))
+    running = True  # Initialize the running variable for the game loop
 
-    # Draw Slide A on the screen
-    screen.blit(Slide, (Slide_x, Slide_y))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == audio_end_event.type:
+                # The audio has ended; restart it
+                pygame.mixer.music.play()
 
-    # Draw Slide B on the screen
-    screen.blit(Slide2, (Slide2_x, Slide2_y))
+        # Event handling for motion of Slides
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:  # Slide A goes up
+            Slide_y -= 0.5
+        if keys[pygame.K_s]:  # Slide A goes down
+            Slide_y += 0.5
+        if keys[pygame.K_UP]:  # Slide B goes up
+            Slide2_y -= 0.5
+        if keys[pygame.K_DOWN]:  # Slide B goes down
+            Slide2_y += 0.5
 
-    # Update the display
-    pygame.display.flip()
+        # Ensure that the slides stay within the screen boundaries
+        Slide_y = max(0, min(Slide_y, height - Slide1_height))
+        Slide2_y = max(0, min(Slide2_y, height - Slide2_height))
 
-# Quit Pygame
-pygame.quit()
+        # Clear the screen with the black background
+        screen.fill(background_color)
+
+        # Draw the ball on the screen in red
+        screen.blit(ball, (ball_x, ball_y))
+
+        # Draw Slide A on the screen in red
+        screen.blit(Slide, (Slide_x, Slide_y))
+
+        # Draw Slide B on the screen in red
+        screen.blit(Slide2, (Slide2_x, Slide2_y))
+
+        # Update the display
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    audio_thread = threading.Thread(target=play_audio)
+    audio_thread.start()
+
+    main()
+
+    # Quit Pygame
+    pygame.quit()
